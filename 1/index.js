676afbe060734d1a8ba7f1ca741d2c4193e19390
刘少杰 <liushaojie@cvte.com>
2020-10-20 13:37:11 +0800

    feat: 目录结构

---
+++ 1/index.js
@@ -0,0 +1,6 @@
+require('./src/math/vector.js')
+require('./src/math/matrix.js')
+
+require('./src/render/vertex.js')
+require('./src/render/color.js')
+require('./src/render/canvas.js')
---
+++ 1/src/render/canvas.js
@@ -0,0 +1,10 @@
+class GCanvas {
+  constructor(canvas) {
+    this.canvas = canvas
+    this.ctx = canvas.getContext('2d')
+  }
+
+  drawline() {
+
+  }
+}
